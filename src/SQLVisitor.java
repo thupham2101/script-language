9670e050c0b02f5823d320e04716d6350229fdc2
thupham2101 <thupham2101@gmail.com>
2019-08-22 03:43:46 +0700

    Do Statement Tree

--- src/SQLVisitor.java
+++ src/SQLVisitor.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
 import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.StatementVisitor;
 import net.sf.jsqlparser.statement.insert.Insert;
+import net.sf.jsqlparser.statement.update.Update;;
 
 public class SQLVisitor implements ExpressionParserVisitor{
 
@@ -16,6 +18,18 @@ public class SQLVisitor implements ExpressionParserVisitor{
         }
         else if(node instanceof ASTcreateStatement) {
             return visit((ASTcreateStatement) node, data);
+        }
+        else if(node instanceof ASTdoExactly) {
+            return visit((ASTdoExactly) node, data);
+        }
+        else if(node instanceof ASTdoAtMost) {
+            return visit((ASTdoAtMost) node, data);
+        }
+        else if(node instanceof ASTProperty) {
+            return visit((ASTProperty) node, data);
+        }
+        else if(node instanceof ASTValue) {
+            return visit((ASTValue) node, data);
         }
         else if(node instanceof ASTTable) {
             return visit((ASTTable) node, data);
@@ -59,6 +73,46 @@ public class SQLVisitor implements ExpressionParserVisitor{
     }
 
     @Override
+    public Object visit(ASTdoExactly node, Object data) {
+        Integer number = (Integer) visit((SimpleNode) node.jjtGetChild(0), data);
+        String table = (String) visit((SimpleNode) node.jjtGetChild(1), data);
+        String property = (String) visit((SimpleNode) node.jjtGetChild(2), data);
+        String value = (String) visit((SimpleNode) node.jjtGetChild(3), data);
+        String output = (String) data;
+
+        for(int i = 0; i < number; i++) {
+     //   	Update update = new Update();
+     //   	update.setTable(new Table(null,table));
+    //        ExpressionList expList = new ExpressionList();
+    //        expList.setExpressions(new ArrayList<>());
+    //        update.accept((StatementVisitor) expList);
+      //      output = output.concat(concat("update").concat("\n");
+        	output = output.concat("update exactly " + table + " set" + property + " = " + value + "\n");
+        }
+        return output;
+    }
+    
+    @Override
+    public Object visit(ASTdoAtMost node, Object data) {
+        Integer number = (Integer) visit((SimpleNode) node.jjtGetChild(0), data);
+        String table = (String) visit((SimpleNode) node.jjtGetChild(1), data);
+        String property = (String) visit((SimpleNode) node.jjtGetChild(2), data);
+        String value = (String) visit((SimpleNode) node.jjtGetChild(3), data);
+        String output = (String) data;
+
+        for(int i = 0; i < number; i++) {
+     //   	Update update = new Update();
+     //   	update.setTable(new Table(null,table));
+    //        ExpressionList expList = new ExpressionList();
+    //        expList.setExpressions(new ArrayList<>());
+    //        update.accept((StatementVisitor) expList);
+      //      output = output.concat(concat("update").concat("\n");
+        	output = output.concat("update at most" + table + " set " + property + " = " + value + "\n");
+        }
+        return output;
+    }
+    
+    @Override
     public Object visit(ASTNumber node, Object data) {
         return node.data.get("value");
     }
@@ -67,5 +121,17 @@ public class SQLVisitor implements ExpressionParserVisitor{
     public Object visit(ASTTable node, Object data) {
         return node.data.get("value");
     }
+    
+    @Override
+    public Object visit(ASTProperty node, Object data) {
+        return node.data.get("value");
+    }
+    
+    @Override
+    public Object visit(ASTValue node, Object data) {
+        return node.data.get("value");
+    }
+    
+
 
 }
